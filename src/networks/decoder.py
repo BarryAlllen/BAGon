d3fbd36f61b719cd9a57bafa156ea9c6520b36a8
Barry <barrryx@outlook.com>
2025-02-21 00:15:58 +0800

    fix: decoder & extractor

--- src/networks/decoder.py
+++ src/networks/decoder.py
@@ -4,63 +4,54 @@ from src.networks.attention import ConvolutionalBlockAttentionModule
 from src.networks.common import ConvBatchNormReluBlock, ResidualBlock
 
 
-class Decoder(nn.Module):
-    def __init__(self, in_channels=3):
+class Extractor(nn.Module):
+    def __init__(self, in_channels):
         super().__init__()
-        # self.attention = ConvolutionalBlockAttentionModule(64)
-        #
-        # self.conv_block_1 = ConvBatchNormReluBlock(in_channels=64, out_channels=64)
-        # self.conv_block_2 = ConvBatchNormReluBlock(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0, bias=False)
-        #
-        # self.residual_block_1 = nn.Sequential(
-        #     ConvBatchNormReluBlock(in_channels=in_channels, out_channels=64),
-        #     ConvBatchNormReluBlock(in_channels=64, out_channels=64),
-        #     ConvBatchNormReluBlock(in_channels=64, out_channels=64),
-        #     ResidualBlock(in_channels=64, out_channels=64, stride=1),
-        #     ResidualBlock(in_channels=64, out_channels=64, stride=1),
-        #     ResidualBlock(in_channels=64, out_channels=64, stride=1)
-        # )
-        #
-        # self.residual_block_2 = nn.Sequential(
-        #     ResidualBlock(in_channels=64, out_channels=64, stride=1),
-        #     ResidualBlock(in_channels=64, out_channels=64, stride=2)
-        # )
-        #
-        # self.residual_block_3 = nn.Sequential(
-        #     ResidualBlock(in_channels=64, out_channels=64, stride=1),
-        #     ResidualBlock(in_channels=64, out_channels=64, stride=2)
-        # )
-        #
-        # self.residual_block_4 = nn.Sequential(
-        #     ResidualBlock(in_channels=64, out_channels=64, stride=1),
-        #     ResidualBlock(in_channels=64, out_channels=64, stride=2)
-        # )
-
-        self.layers = nn.Sequential(
-            ConvBatchNormReluBlock(in_channels=in_channels, out_channels=64),
-            ConvBatchNormReluBlock(in_channels=64, out_channels=64),
-            ConvBatchNormReluBlock(in_channels=64, out_channels=64),
-            ResidualBlock(in_channels=64, out_channels=64, stride=1),
-            ResidualBlock(in_channels=64, out_channels=64, stride=1),
-            ResidualBlock(in_channels=64, out_channels=64, stride=1),
-            ConvBatchNormReluBlock(in_channels=64, out_channels=64),
-            ResidualBlock(in_channels=64, out_channels=64, stride=1),
-            ResidualBlock(in_channels=64, out_channels=64, stride=2),
-            ResidualBlock(in_channels=64, out_channels=64, stride=1),
-            ResidualBlock(in_channels=64, out_channels=64, stride=2),
-            ResidualBlock(in_channels=64, out_channels=64, stride=1),
-            ResidualBlock(in_channels=64, out_channels=64, stride=2),
-            ConvolutionalBlockAttentionModule(64),
-            ConvBatchNormReluBlock(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0, bias=False)
+        self.layer1 = ConvBatchNormReluBlock(in_channels, 64, 3, 1)
+        self.layer2 = nn.Sequential(
+            ResidualBlock(64, 64, 1),
+            ResidualBlock(64, 64, 2)
         )
-
-        self.linear = nn.Linear(in_features=256, out_features=30)
+        self.layer3 = nn.Sequential(
+            ResidualBlock(64, 64, 1),
+            ResidualBlock(64, 64, 2)
+        )
+        self.layer4 = nn.Sequential(
+            ResidualBlock(64, 64, 1),
+            ResidualBlock(64, 64, 2)
+        )
+        self.layer5 = nn.Conv2d(64, 1, kernel_size=1, stride=1, padding=0, bias=False)
+        self.linear = nn.Linear(256, 30)
+        self.attention = ConvolutionalBlockAttentionModule(64)
 
     def forward(self, x):
-        x = self.layers(x)
-        x.squeeze_(1)
-        x = x.view(-1, 1, 256)
-        out = self.linear(x)
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.attention(out)
+        out = self.layer5(out)
+        out.squeeze_(1)
+        out = out.view(-1, 1, 256)
+        out = self.linear(out)
         out.squeeze_(1)
         return out
 
+
+class Decoder(nn.Module):
+    def __init__(self, in_channels=3):
+        super().__init__()
+        self.layer1 = nn.Sequential(
+            ConvBatchNormReluBlock(in_channels, 64, 3, 1),
+            ConvBatchNormReluBlock(64, 64, 3, 1),
+            ConvBatchNormReluBlock(64, 64, 3, 1),
+            ResidualBlock(64, 64, 3, 1),
+            ResidualBlock(64, 64, 3, 1),
+            ResidualBlock(64, 64, 3, 1),
+        )
+        self.extractor = Extractor(in_channels=64)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.extractor(out)
+        return out
