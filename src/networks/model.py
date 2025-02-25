93d753083ac75a3d26a1d2bde6351205ba99f207
Barry <barrryx@outlook.com>
2025-02-25 14:19:05 +0800

    fix: turn off torch.autograd.set_detect_anomaly

--- src/networks/model.py
+++ src/networks/model.py
@@ -6,7 +6,8 @@ from src.networks.decoder import Decoder
 from src.networks.encoder import Encoder
 from src.networks.noise_layers import ScreenShootingNoiseLayer, NoneNoiseLayer
 
-torch.autograd.set_detect_anomaly(True)
+# torch.autograd.set_detect_anomaly(True)
+
 class BAGon(nn.Module):
     def __init__(self, in_channels=3, out_channels=3, is_noise=True):
         super().__init__()
