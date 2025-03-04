746d8c0312c12be146277e2a974cb1c2223cf303
Barry <barrryx@outlook.com>
2025-03-04 11:20:05 +0800

    fix: separate load weights

--- src/utils/model_loader.py
+++ src/utils/model_loader.py
@@ -1,17 +1,48 @@
+
+import os
 import torch
 
+from torch import nn
+from src.networks.discriminator import Discriminator
 from src.networks.model import BAGon
 
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-def get_model(params_path, device):
+
+def get_model(weights_path: str, device):
     model = BAGon().to(device)
+    model = load_weights(model, weights_path, device)
+    return model
 
-    checkpoint = torch.load(params_path)
 
+def get_discriminator(weights_path: str, device):
+    discriminator = Discriminator().to(device)
+    discriminator = load_weights(discriminator, weights_path, device)
+    return discriminator
+
+
+def load_weights(model: nn.Module, weights_path: str, device):
+    # load weights
+    checkpoint = torch.load(weights_path, map_location=device)
     weights_dict = {}
     for k, v in checkpoint.items():
         new_k = k.replace('module.', '') if 'module' in k else k
+        if v.device.type != device.type:  # Check if weights
+            raise RuntimeError(f"Weight {new_k} is not on device {device}, but on {new_k.device} instead.")
         weights_dict[new_k] = v
-
     model.load_state_dict(weights_dict)
     return model
+
+
+# def main():
+#     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#     model = get_model(
+#         weights_path='/workspace/code/watermark/bagon/data/result/benchmark/model/model.pth',
+#         device=device
+#     )
+#     print(type(model))
+#     print(next(model.parameters()).device)
+#
+#
+# if __name__ == '__main__':
+#     main()
