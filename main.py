a0d1805e6becbde3cbfdad08662ae82f59045d08
Barry <barrryx@outlook.com>
2025-03-05 14:27:09 +0800

    feat: encoder v0 & decoder v0

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--num_workers", type=int, default=56)
     parser.add_argument("--epochs", type=int, default=100)
-    parser.add_argument("--seed", type=int, default=3504)
+    parser.add_argument("--seed", type=int, default=3502)
     parser.add_argument("--warmup", type=int, default=3)
     parser.add_argument("--model_save_step", type=int, default=10)
     parser.add_argument("--epoch_show_step", type=int, default=1)
--- src/networks/decoder.py
+++ src/networks/decoder.py
@@ -60,7 +60,60 @@ class Decoder(nn.Module):
         return out
 
 
-class ExtractorV2(nn.Module):
+class ExtractorV0(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.layer1 = ConvBatchNormReluBlock(in_channels, 64, 3, 1)
+        self.layer2 = nn.Sequential(
+            ResidualBlock(64, 64, 1),
+            ResidualBlock(64, 64, 2)
+        )
+        self.layer3 = nn.Sequential(
+            ResidualBlock(64, 64, 1),
+            ResidualBlock(64, 64, 2)
+        )
+        self.layer4 = nn.Sequential(
+            ResidualBlock(64, 64, 1),
+            ResidualBlock(64, 64, 2)
+        )
+        self.layer5 = nn.Conv2d(64, 1, kernel_size=1, stride=1, padding=0, bias=False)
+        self.linear = nn.Linear(256, 30)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out.squeeze_(1)
+        out = out.view(-1, 1, 256)
+        out = self.linear(out)
+        out.squeeze_(1)
+        return out
+
+
+class DecoderV0(nn.Module):
+    def __init__(self, in_channels=3):
+        super().__init__()
+        self.name = f"Model: {self.__class__.__name__}"
+
+        self.layer1 = nn.Sequential(
+            ConvBatchNormReluBlock(in_channels, 64, 3, 1),
+            ConvBatchNormReluBlock(64, 64, 3, 1),
+            ConvBatchNormReluBlock(64, 64, 3, 1),
+            ResidualBlock(64, 64, 1),
+            ResidualBlock(64, 64, 1),
+            ResidualBlock(64, 64, 1),
+        )
+        self.extractor = Extractor(in_channels=64)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.extractor(out)
+        return out
+
+
+class ExtractorV1(nn.Module):
     def __init__(self, in_channels):
         super().__init__()
         self.layer1 = ConvBatchNormReluBlock(in_channels, 64, 3, 1)
@@ -97,7 +150,7 @@ class ExtractorV2(nn.Module):
         return out
 
 
-class DecoderV2(nn.Module):
+class DecoderV1(nn.Module):
     def __init__(self, in_channels=3, hidden_channels=64):
         super().__init__()
         self.name = f"Model: {self.__class__.__name__}"
@@ -110,7 +163,7 @@ class DecoderV2(nn.Module):
             ResidualBlock(in_channels=hidden_channels, out_channels=hidden_channels, stride=1),
             ResidualBlock(in_channels=hidden_channels, out_channels=hidden_channels, stride=1),
         )
-        self.extractor = ExtractorV2(in_channels=hidden_channels)
+        self.extractor = ExtractorV1(in_channels=hidden_channels)
 
     def forward(self, x):
         out = self.layer1(x)
